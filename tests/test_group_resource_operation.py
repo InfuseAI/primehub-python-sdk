209d9b26b7413f1c224fb4b90321d0f5af593453
Ching Yi, Chan <chingyichan.tw@gmail.com>
2021-07-28 07:50:16 +0800

    Fix tests
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- tests/test_group_resource_operation.py
+++ tests/test_group_resource_operation.py
@@ -27,9 +27,8 @@ class TestGroupResourceOperation(BaseTestCase):
         self.sdk.register_command('test_group_resource_operation', GroupFoobarCommand)
 
     def test_group_not_found(self):
-        with self.assertRaises(GroupIsRequiredException) as ctx:
-            self.cli_stdout(['app.py', 'test_group_resource_operation', 'list'])
-        self.assertEqual('No group information, please configure the active group first.', str(ctx.exception))
+        output = self.cli_stderr(['app.py', 'test_group_resource_operation', 'list'])
+        self.assertEqual('No group information, please configure the active group first.', output.strip())
 
     def test_group_resource_operation(self):
         self.sdk.primehub_config.group_info = {'name': 'phusers'}
