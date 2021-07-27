1d30b48adc7f8b3a8cd5aa9eb7d4f9812f2d7fef
Even Wei <evenwei@infuseai.io>
2021-07-27 17:23:42 +0800

    Add jobs rerun and cancel command
    Signed-off-by: Even Wei <evenwei@infuseai.io>

--- primehub/jobs.py
+++ primehub/jobs.py
@@ -67,10 +67,36 @@ class Jobs(Helpful, Module):
           'where': {
             'groupId_in': [self.primehub_config.group_info['id']],
             'id': job_id
-          },
+          }
         }
         results = self.request(variables, Jobs.query)
         return results['data']['phJobsConnection']['edges']
+
+    # TODO: handel id does not exist
+    @cmd(name='rerun', description='rerun job by id')
+    def rerun(self, job_id):
+        query = """
+        mutation ($where: PhJobWhereUniqueInput!) {
+          rerunPhJob(where: $where) {
+            id
+          }
+        }
+        """
+        results = self.request({'where': {'id': job_id}}, query)
+        return results['data']
+
+    # TODO: handel id does not exist
+    @cmd(name='cancel', description='rerun job by id')
+    def cancel(self, job_id):
+        query = """
+        mutation ($where: PhJobWhereUniqueInput!) {
+          cancelPhJob(where: $where) {
+            id
+          }
+        }
+        """
+        results = self.request({'where': {'id': job_id}}, query)
+        return results['data']
 
     def help_description(self):
         return "Get a job or list jobs"
